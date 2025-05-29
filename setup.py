e05e38b7658e7b91f47192cd0e723472bfd8b478
AtharvaBOT7 <atharvachundurwar841@gmail.com>
2025-05-28 18:28:18 -0700

    local package named src added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical Chatbot",
+    version = '0.0.0',
+    author = "Atharva Chundurwar",
+    author_email = "atharvachundurwar841@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
